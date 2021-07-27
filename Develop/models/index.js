126856fe2126f6dfe5e9030d0e28a16dbe50b8b4
AnthonySzymczak <doyalajs1@live.com>
2021-07-27 18:45:50 -0400

    tag-routes

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -10,7 +10,7 @@ Product.belongsTo(Category, {
 });
 
 // Categories have many Products
-
+Category.hasMany(Product);
 // Products belongToMany Tags (through ProductTag)
 
 // Tags belongToMany Products (through ProductTag)
--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -3,26 +3,61 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  //console.log('Hello');
+const findAllTags = await Tag.findAll({
+  include: [{model: Product, through: ProductTag}]
+})
+res.json(findAllTags);
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  //console.log('Hello');
+
+  const findOneTag = await Tag.findOne({
+    where:{
+      id: req.params.id
+    },
+    include: [{model: Product, through: ProductTag}]
+  })
+  res.json(findOneTag);
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
+  //console.log('Hello');
+  const createNewTag = await Tag.create({
+    tag_name: req.params.tag_name
+  })
+res.json(createNewTag);
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  //console.log('Hello');
+  
+  const updateTagName = await Tag.update(req.body, {
+    where: {
+      id:req.params.id
+    }
+  })
+res.json(updateTagName);
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
+  //console.log('Hello');
+  
+  const deleteTagByID = await Tag.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+res.json(deleteTagByID);
 });
 
 module.exports = router;
