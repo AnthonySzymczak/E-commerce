30dcb0ade26948e622f4778fb26846f5b066ba6c
AnthonySzymczak <doyalajs1@live.com>
2021-07-27 10:22:25 -0400

    DB models pt1

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -3,9 +3,15 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  console.log('Hello')
+  const categories = await Category.findAll({
+    include:
+    [Product]
+  })
+  res.json(categories)
 });
 
 router.get('/:id', (req, res) => {
