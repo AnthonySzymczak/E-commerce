3a9cf2bd95caa4186f31cff4139f1bf02e0a79d3
AnthonySzymczak <doyalajs1@live.com>
2021-07-27 12:12:10 -0400

    api route creation

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -6,29 +6,63 @@ const { Category, Product } = require('../../models');
 router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
-  console.log('Hello')
+    // console.log('Hello')
+  
   const categories = await Category.findAll({
     include:
     [Product]
   })
-  res.json(categories)
+  res.json(categories);
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+    // console.log('Hello')
+
+  const byID = await Category.findOne({
+    where: {
+      id: req.params.id
+    },
+    include:
+    [Product]
+  })
+res.json(byID);
+
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+    // console.log('Hello')
+  const createNewCategory = await Category.create({
+    category_name: req.body.category_name
+  })
+res.json(createNewCategory);
+
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+    // console.log('Hello')
+  const categoryUpdate = await Category.update(req.body, {
+    where:{
+      id: req.params.id
+    }
+  })
+res.json(categoryUpdate);
+
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+    // console.log('Hello')
+    const categoryDelete = await Category.destroy({
+      where:{
+        id: req.params.id
+      }
+    })
+res.json(categoryDelete);
+
 });
 
 module.exports = router;
